1c42f7566d059c44494ff777e0f5b54ef22b0aac
ifk <ifk.rwth@gamil.com>
2022-06-13 22:31:22 +0200

    fit in chunks so the queue doesnt become too large

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from multiprocessing import Process, Queue
 
 from rsc.diffpyfit import DiffpyFit
 from rsc.util import read_config
+import numpy as np
 from glob import glob
 
+from datetime import datetime
+startTime = datetime.now()
 
 DAT_DIR = './dat/'
 OUT_DIR = './res/'
@@ -17,13 +20,16 @@ def multi_run(file):
 if __name__ == "__main__":
     conf = read_config(CONF_PATH)
     Fit = DiffpyFit(conf, ['Ni', 'NiO'], ['sphericalCF', 'sphericalCF'])
-    gr_files = glob(f'{DAT_DIR}*.gr')
+    data_files = glob(f'{DAT_DIR}*.gr')
     queue = Queue()
 
-    processes = [Process(target=multi_run, args=(x,)) for x in gr_files[:100]]
+    for chunk in np.array_split(data_files, len(data_files)//64):
+        processes = [Process(target=multi_run, args=(df,)) for df in chunk]
 
-    for p in processes:
-        p.start()
+        for p in processes:
+            p.start()
 
-    for p in processes:
-        p.join()
+        for p in processes:
+            p.join()
+
+    print(datetime.now() - startTime)
