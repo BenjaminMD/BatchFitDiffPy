317f480ea6d202b83efbf37707d322f4bde7e3b2
ifk <ifk.rwth@gamil.com>
2022-06-13 20:59:58 +0200

    cleaned up old files. working version.

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ def multi_run(file):
     Fit.run_simple_fit(file, OUT_DIR)
 
 
-
 if __name__ == "__main__":
     conf = read_config(CONF_PATH)
-    Fit = DiffpyFit(conf, ['Ni'], ['sphericalCF'])
-    gr_files = glob(DAT_DIR + '*.gr')
+    Fit = DiffpyFit(conf, ['Ni', 'NiO'], ['sphericalCF', 'sphericalCF'])
+    gr_files = glob(f'{DAT_DIR}*.gr')
     queue = Queue()
 
-    processes = [Process(target=multi_run, args=(x,)) for x in gr_files]
+    processes = [Process(target=multi_run, args=(x,)) for x in gr_files[:100]]
 
     for p in processes:
         p.start()
--- rsc/util.py
+++ rsc/util.py
@@ -5,3 +5,38 @@ def read_config(config_path):
     with open(config_path) as f:
         dat = json.load(f)
     return dat
+
+# def remove_phase(self, p):
+#     if p not in self.phases:
+#         raise ValueError(f'{p} is not in phases')
+#     self.phases.remove(p)
+
+#     f = self.p_f[p]
+
+#     # remove phase from equation
+#     equation_list = self.equation.split(' + ')
+#     equation_list.remove(f'{p}Γ * {p}Γ{f}')
+#     self.equation = ' + '.join(equation_list)
+
+#     # remove phase from cif_files
+#     self.cif_files.pop(f'{p}Γ')
+
+#     # remove phase from functions
+#     self.functions.pop(f'{p}Γ{f}')
+
+# def add_phase(self, p):
+#     if p in self.phases:
+#         raise ValueError(f'{p} is already in phases')
+#     self.phases.append(p)
+#     f = self.p_f[p]
+
+#     # add phase to equation
+#     equation_list = self.equation.split(' + ')
+#     equation_list.append(f'{p}Γ * {p}Γ{f}')
+#     self.equation = ' + '.join(equation_list)
+
+#     # add phase to cif_files
+#     self.cif_files[f'{p}Γ'] = f'{p}.cif'
+
+#     # add phase to functions
+#     self.functions[f'{p}Γ{f}'] = self.fetch_function(p ,self.p_f[p])
